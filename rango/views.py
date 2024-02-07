abadd09b84cb5b56b426e6a83702619fb4683512
Ian A'Hara <2756719a@student.gla.ac.uk>
2024-02-07 19:55:30 +0000

    task 8 with exercise

--- rango/views.py
+++ rango/views.py
@@ -62,7 +62,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     
@@ -75,7 +75,7 @@ def add_page(request, category_name_slug):
         category = None
 # You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     form = PageForm()
     if request.method == 'POST':
         form = PageForm(request.POST)
